18c8c6ab54fd41698a52eef4496b813b32224d2e
Jamey Nakama <jnakama@oreilly.com>
2024-01-13 13:25:32 -0800

    Add limit flag

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
+	repoLimit := flag.Int("l", 25, "limit of repositories to scrape")
+	flag.Parse()
+
+	var reposVisited int
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("github.com"),
 	)
 
 	c.OnHTML("h2.h3.lh-condensed > a[href]", func(e *colly.HTMLElement) {
-		fmt.Println(e.Attr("href"))
+		reposVisited++
+		if reposVisited > *repoLimit {
+			return
+		}
+
+		fmt.Printf("Will scrape %s\n", e.Request.AbsoluteURL(e.Attr("href")))
 	})
 
 	c.OnRequest(func(r *colly.Request) {
