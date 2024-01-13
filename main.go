bea4f294c273226932399587c9d0f47f413103bd
Jamey Nakama <jnakama@oreilly.com>
2024-01-13 13:53:13 -0800

    Set up separate scraper for each repo

--- main.go
+++ main.go
@@ -3,36 +3,74 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
+
+type cfg struct {
+	repoLimit int
+}
+
+type scrapeHelper struct {
+	toScrape []string
+	scraped  []string
+}
+
+func (s *scrapeHelper) getTrendingRepos(cfg cfg) {
+	c := colly.NewCollector()
+
+	c.OnHTML("h2.h3.lh-condensed > a[href]", func(e *colly.HTMLElement) {
+		if len(s.toScrape) >= cfg.repoLimit {
+			return
+		}
+		link := e.Attr("href")
+		s.toScrape = append(s.toScrape, e.Request.AbsoluteURL(link))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Getting repos to scrape...")
+	})
+
+	c.Visit("https://github.com/trending")
+}
+
+func (s *scrapeHelper) scrapeReadme(url string) {
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Scraping %s...\n", r.URL.String())
+	})
+
+	c.OnHTML("span.author > a[href]", func(e *colly.HTMLElement) {
+		fmt.Println("\tAuthor: " + strings.TrimSpace(e.Text))
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("\tFinished scraping " + r.Request.URL.String())
+		s.scraped = append(s.scraped, r.Request.URL.String())
+	})
+
+	c.Visit(url)
+}
 
 func main() {
 	repoLimit := flag.Int("l", 25, "limit of repositories to scrape")
 	flag.Parse()
 
-	var reposVisited int
+	cfg := cfg{
+		repoLimit: *repoLimit,
+	}
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("github.com"),
-	)
-
-	c.OnHTML("h2.h3.lh-condensed > a[href]", func(e *colly.HTMLElement) {
-		reposVisited++
-		if reposVisited > *repoLimit {
-			return
-		}
-
-		fmt.Printf("Will scrape %s\n", e.Request.AbsoluteURL(e.Attr("href")))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Printf("Visiting %s...\n-----\n", r.URL)
-	})
-
-	run(c)
+	run(cfg)
 }
 
-func run(c *colly.Collector) {
-	c.Visit("https://github.com/trending")
+func run(cfg cfg) {
+	var sh scrapeHelper
+
+	sh.getTrendingRepos(cfg)
+
+	for _, repo := range sh.toScrape {
+		sh.scrapeReadme(repo)
+	}
 }
